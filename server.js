a0158b3621ade52242cd4e31cd7129c2766c88a7
Javier Morilla <javiermorillab@gmail.com>
2023-10-04 21:57:19 +0200

    Fix Heroku deployment

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/', router);
 
 app.use('/assets', express.static('assets'));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
