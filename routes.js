bc01da158a281f4838c911e15ecbafe485ba9430
Javier Morilla <javiermorillab@gmail.com>
2023-11-13 23:14:08 +0100

    Questions changes

--- routes.js
+++ routes.js
@@ -5,16 +5,16 @@ const questions = [
   {
     id: 1,
     question:
-      "You do this when you need to enter data into your computer using the keyboard.",
-    answer: "Type",
-    options: ["Drag", "Paste", "Copy", "Type"].sort(() => Math.random() - 0.5),
+      "The person __________ always listens to me is my best friend.",
+    answer: "Who",
+    options: ["Who", "Whom", "Whose", "Which"].sort(() => Math.random() - 0.5),
     nextQR: "2",
   },
   {
     id: 2,
-    question: "Which phrasal verb mean “understand”?",
-    answer: "Make up",
-    options: ["Blow up", "Keep on", "Turn into", "Make up"].sort(
+    question: "We should believe in _________ when facing challenges.",
+    answer: "Ourselves",
+    options: ["Ourselves", "Themselves", "Herself", "Himself"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "3",
@@ -22,9 +22,9 @@ const questions = [
   {
     id: 3,
     question:
-      "How does someone feel when they've just seen a really impressive magic trick?",
-    answer: "Astonished",
-    options: ["Bored", "Interesting", "Annoyed", "Astonished"].sort(
+      "My sister and I help ___________ with homework every evening.",
+    answer: "Each other",
+    options: ["Each other", "Ourselves", "One another", "Themselves"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "4",
@@ -32,26 +32,26 @@ const questions = [
   {
     id: 4,
     question:
-      "If I'm continuously listening to a song on repeat, which phrasal verb describes my action?",
-    answer: "Keep on",
-    options: ["Blow up", "Make up", "Come out", "Keep on"].sort(
+      "Someone who always tells the truth is considered to be very ________.",
+    answer: "Honest",
+    options: ["Loyal", "Honest", "Sociable", "Tolerant"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "5",
   },
   {
     id: 5,
-    question: "Which of these is a state verb: eat, love, or swim?",
-    answer: "Love",
-    options: ["Eat", "Love", "Swim"].sort(() => Math.random() - 0.5),
+    question: "A __________ person is someone who is easy to talk to and approachable.",
+    answer: "Friendly",
+    options: ["Mature", "Friendly", "Reliable", "Honest"].sort(() => Math.random() - 0.5),
     nextQR: "6",
   },
   {
     id: 6,
     question:
-      "When a secret or new product becomes publicly known, which phrasal verb is used?",
-    answer: "Come out",
-    options: ["Turn into", "Make up", "Blow up", "Come out"].sort(
+      "A __________ friend will stand by you in good times and bad.",
+    answer: "Loyal",
+    options: ["Mature", "Loyal", "Patient", "Kind"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "7",
@@ -59,9 +59,9 @@ const questions = [
   {
     id: 7,
     question:
-      "Which adjective describes someone who is no longer excited because they've seen or done something too many times?",
-    answer: "Bored",
-    options: ["Amused", "Astonished", "Amazed", "Bored"].sort(
+      "A __________ person often goes out of their way to help others.",
+    answer: "Kind",
+    options: ["Sociable", "Kind", "Responsible", "Tolerant"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "8",
@@ -69,9 +69,9 @@ const questions = [
   {
     id: 8,
     question:
-      "Where would you find a list of websites you've recently visited?",
-    answer: "Web browser",
-    options: ["Cursor", "Window", "Attachment", "Web browser"].sort(
+      "Someone who acts in a grown-up and thoughtful way is considered __________.",
+    answer: "Mature",
+    options: ["Mature", "Reliable", "Friendly", "Honest"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "9",
@@ -79,22 +79,22 @@ const questions = [
   {
     id: 9,
     question:
-      "Complete the sentence with the correct tense: 'Every day, she ______ (browse) the internet, but right now she ______ (download) a new software.'",
-    answer: "browses, is downloading",
+      "A __________ person doesn't get easily upset or annoyed.",
+    answer: "Patient",
     options: [
-      "browses, is downloading",
-      "browse, download",
-      "browse, downloads",
-      "browses, downloads",
+      "Sociable",
+      "Responsible",
+      "Loyal",
+      "Patient",
     ].sort(() => Math.random() - 0.5),
     nextQR: "10",
   },
   {
     id: 10,
     question:
-      "How does one feel when they've heard a very scary noise in the night?",
-    answer: "Frightened",
-    options: ["Pleased", "Relaxed", "Interested", "Frightened"].sort(
+      "If you can count on someone to do what they say, they are __________.",
+    answer: "Reliable",
+    options: ["Reliable", "Mature", "Honest", "Sociable"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "11",
@@ -102,18 +102,18 @@ const questions = [
   {
     id: 11,
     question:
-      "If a balloon fills with too much air and bursts, which phrasal verb is used?",
-    answer: "Blow up",
-    options: ["Make up", "Come out", "Keep on", "Blow up"].sort(
+      "A __________ person takes care of their duties and obligations.",
+    answer: "Responsible",
+    options: ["Tolerant", "Responsible", "Kind", "Loyal"].sort(
       () => Math.random() - 0.5
     ),
   },
   {
     id: 12,
     question:
-      "What do you call the text or item that takes you to another webpage or file when clicked?",
-    answer: "Link",
-    options: ["Link", "Hashtag", "Folder", "Window"].sort(
+      "Being __________ means being accepting of others' differences.",
+    answer: "Tolerant",
+    options: ["Tolerant", "Loyal", "Reliable", "Kind"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "13",
@@ -121,17 +121,17 @@ const questions = [
   {
     id: 13,
     question:
-      "If you want to 'make it big' in the business world, you might eventually be _____ (rich beyond measure).",
-    answer: "Roll in it",
-    options: ["Make a living", "Roll in it"].sort(() => Math.random() - 0.5),
+      "An __________ is someone you know but not very well.",
+    answer: "Acquaintance",
+    options: ["Acquaintance", "Enemy", "Best friend", "Sibling"].sort(() => Math.random() - 0.5),
     nextQR: "14",
   },
   {
     id: 14,
     question:
-      "What do you call the bar or arrow you use to select things on a computer screen?",
-    answer: "Cursor",
-    options: ["Cursor", "Password", "Hard drive", "Folder"].sort(
+      "A __________ is someone you trust and share your deepest secrets with.",
+    answer: "Best friend",
+    options: ["Best friend", "Colleague", "Classmate", "Flatmate"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "15",
@@ -139,9 +139,9 @@ const questions = [
   {
     id: 15,
     question:
-      "Which adjective describes someone who's just heard a very funny joke?",
-    answer: "Amused",
-    options: ["Amused", "Scared", "Frightened", "Interesting"].sort(
+      "In informal language, a __________ can refer to a friend.",
+    answer: "Mate",
+    options: ["Mate", "Teammate", "Rival", "Roommate"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "16",
@@ -149,9 +149,9 @@ const questions = [
   {
     id: 16,
     question:
-      "How would you feel if you accidentally fell in front of your classmates?",
-    answer: "Embarrassed",
-    options: ["Embarrassed", "Pleased", "Amused"].sort(
+      "A __________ is someone you work with.",
+    answer: "Colleague",
+    options: ["Colleague", "Classmate", "Close friend", "Enemy"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "17",
@@ -159,9 +159,9 @@ const questions = [
   {
     id: 17,
     question:
-      "What's the term for the symbol used to categorize or tag social media posts?",
-    answer: "Hashtag",
-    options: ["Hashtag", "Web browser", "Link", "Icon"].sort(
+      "A __________ is someone with whom you have a strong and intimate relationship.",
+    answer: "Close friend",
+    options: ["Close friend", "Enemy", "Rival", "Opponent"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "18",
@@ -169,18 +169,18 @@ const questions = [
   {
     id: 18,
     question:
-      "If you were to earn money through your job, which phrase means the same as 'earning money'?",
-    answer: "Make a living",
-    options: ["Make a living", "Make it big", "Rolling in it"].sort(
+      "An __________ is a person who is actively opposed to someone.",
+    answer: "Enemy",
+    options: ["Enemy", "Acquaintance", "Best friend", "Teammate"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "19",
   },
   {
     id: 19,
-    question: "If an ice cube is left out in the sun it ____________ water?",
-    answer: "turns into",
-    options: ["turns into", "make up", "come out"].sort(
+    question: "A __________ is a person competing with another for the same objective.",
+    answer: "Rival",
+    options: ["Rival", "Flatmate", "Roommate", "Next door neighbour"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "20",
@@ -188,9 +188,9 @@ const questions = [
   {
     id: 20,
     question:
-      "When a document or file is attached to an email, what do we call that?",
-    answer: "Attachment",
-    options: ["Attachment", "Icon", "Install", "Blog"].sort(
+      "A __________ is someone you share an apartment with.",
+    answer: "Flatmate",
+    options: ["Flatmate", "Travel buddy", "Classmate", "Teammate"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "21",
@@ -198,18 +198,18 @@ const questions = [
   {
     id: 21,
     question:
-      "Which adjective best describes a film that's not only good but keeps you at the edge of your seat?",
-    answer: "Entertaining",
-    options: ["Entertaining", "Interested", "Frightening", "Bored"].sort(
+      "A __________ lives in the residence next to yours.",
+    answer: "Next door neighbour",
+    options: ["Next door neighbour", "Opponent", "Colleague", "Mate"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "22",
   },
   {
     id: 22,
-    question: "How might you feel if you saw a snake while hiking?",
-    answer: "Alarmed",
-    options: ["Alarmed", "Pleased", "Relaxed", "Amused"].sort(
+    question: "A __________ is a member of your family.",
+    answer: "Relative",
+    options: ["Relative", "Teammate", "Stranger", "Travel buddy"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "23",
@@ -217,27 +217,27 @@ const questions = [
   {
     id: 23,
     question:
-      "If you didn't want to use a piece of text or image anymore, what action would you take on your computer?",
-    answer: "Delete",
-    options: ["Delete", "Copy", "Paste", "Download"].sort(
+      "A __________ is your brother or sister.",
+    answer: "Sibling",
+    options: ["Sibling", "Stranger", "Teammate", "Travel buddy"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "23",
   },
   {
     id: 24,
-    question: "Which phrasal verb means to handle a situation or problem?",
-    answer: "Deal with",
-    options: ["Deal with", "Turn into", "Make up", "Blow up"].sort(
+    question: "To spend time in a relaxed way with someone is to __________ them.",
+    answer: "Hang out with",
+    options: ["Hang out with", "Cheer up", "Make you laugh", "Get on with"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "25",
   },
   {
     id: 25,
-    question: "You do this when you want to find something online.",
-    answer: "Search",
-    options: ["Search", "Install", "Download", "Paste"].sort(
+    question: "When you make someone feel better, you __________ them.",
+    answer: "Support",
+    options: ["Support", "Forgive", "Judge", "Cheer up"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "26",
@@ -245,9 +245,9 @@ const questions = [
   {
     id: 26,
     question:
-      "What is the action you take when you want to bring a file from the internet to your device?",
-    answer: "Download",
-    options: ["Download", "Upload", "Search", "Paste"].sort(
+      "The teacher __________ helped me understand math is Mr. Smith.",
+    answer: "Who",
+    options: ["Who", "Whose", "When", "Which"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "27",
@@ -255,22 +255,22 @@ const questions = [
   {
     id: 27,
     question:
-      "Complete the sentence with the right tense: 'She usually _______ (write) in her blog, but today she _______ (make) a video post.'",
-    answer: "writes, is making",
+      "I visited the museum __________ has the famous Van Gogh painting.",
+    answer: "Which",
     options: [
-      "is writing, is making",
-      "writes, is making",
-      "write, make",
-      "writes, makes",
+      "Which",
+      "Where",
+      "Who",
+      "Whose",
     ].sort(() => Math.random() - 0.5),
     nextQR: "28",
   },
   {
     id: 28,
     question:
-      "When I remember my password, I always _______ (enter) it immediately. But right now, I _______ (try) to recall it.",
-    answer: "enter, am trying",
-    options: ["enters, tries", "enter, try", "enter, am trying"].sort(
+      "She's the author __________ latest book won an award.",
+    answer: "Whose",
+    options: ["Whose", "Who", "That", "When"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "29",
@@ -278,9 +278,9 @@ const questions = [
   {
     id: 29,
     question:
-      "What action do you take when you want to make a duplicate of a file or a piece of text?",
-    answer: "Copy",
-    options: ["Copy", "Paste", "Drag", "Delete"].sort(
+      "We went to the restaurant __________ serves Italian cuisine.",
+    answer: "Which",
+    options: ["Which", "Where", "Who", "Whose"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "30",
@@ -288,9 +288,9 @@ const questions = [
   {
     id: 30,
     question:
-      "If someone continuously speaks about the same topic, which phrasal verb can describe their action?",
-    answer: "Keep on",
-    options: ["Deal with", "Turn into", "Blow up", "Keep on"].sort(
+      "I made this cake by __________.",
+    answer: "Myself",
+    options: ["Myself", "Ourselves", "Themselves", "Yourself"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "31",
@@ -298,9 +298,9 @@ const questions = [
   {
     id: 31,
     question:
-      "Which word describes the online diary where people share their personal experiences or opinions?",
-    answer: "Blog",
-    options: ["Blog", "Hashtag", "Folder", "Window"].sort(
+      "They built their house __________.",
+    answer: "Themselves",
+    options: ["Themselves", "Ourselves", "Himself", "Itself"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "32",
@@ -308,9 +308,9 @@ const questions = [
   {
     id: 32,
     question:
-      "When you want to move an icon to another spot on your screen, you _______ it.",
-    answer: "drag",
-    options: ["drag", "install", "download", "upload"].sort(
+      "She taught __________ to play the piano.",
+    answer: "Herself",
+    options: ["Herself", "Myself", "Himself", "Ourselves"].sort(
       () => Math.random() - 0.5
     ),
     nextQR: "33",
@@ -318,13 +318,13 @@ const questions = [
   {
     id: 33,
     question:
-      "Complete the sentence: 'Usually, he _______ (share) photos, but now he _______ (post) an amusing story.'",
-    answer: "shares, is posting",
+      "You should be proud of __________ for doing so well.",
+    answer: "Yourself",
     options: [
-      "shares, is posting",
-      "share, posts",
-      "share, post",
-      "is sharing, is posting",
+      "Yourself",
+      "Themselves",
+      "Himself",
+      "Herself",
     ].sort(() => Math.random() - 0.5),
     nextQR: "end",
   },
